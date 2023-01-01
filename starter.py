6e383c592dd01ad7521432d5cd4a251ef309db3a
aleksandrakalas <aleksandrakalas2@gmail.com>
2023-01-01 18:57:53 -0500

    Added initial file with some documentation in regards to beginning of project

---
+++ starter.py
@@ -0,0 +1,9 @@
+"""
+The project is to begin as a simple mood tracker. It will ask everyday the mood of the individual and input it into the library of previous moods.
+
+Later on developments will be made to produce statistics regarding the weekly, monthly, and yearly count ups of the moods.
+
+Following this, activites that were done during the day can also be analyzed.
+
+Finally a GUI to make the mood tracker interactive for the user.
+"""
