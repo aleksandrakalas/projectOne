bfa8644e9cf4a576b74ce72b96753c7019d569d0
aleksandrakalas <aleksandrakalas2@gmail.com>
2023-01-01 19:12:31 -0500

    Introduced function for user input

--- starter.py
+++ starter.py
@@ -1,3 +1,4 @@
+import datetime
 """
 The project is to begin as a simple mood tracker. It will ask everyday the mood of the individual and input it into the library of previous moods.
 
@@ -6,4 +7,22 @@ Later on developments will be made to produce statistics regarding the weekly, m
 Following this, activites that were done during the day can also be analyzed.
 
 Finally a GUI to make the mood tracker interactive for the user.
+Aleksandra Kalas
+Created: 01/01/2023
+Completed: --/--/----
 """
+# Dictionary to keep track of moods that were inputted by user
+moods = {'Jan':[], 'Feb': [], 'Mar': [], 'Apr': [], 'May': [], 'Jul': [], 'Aug': [], 'Sep': [], 'Oct': [], 'Nov': [], 'Dec': []}
+
+def userInput() -> None:
+    """
+    Ask for user input for their mood and add to the corresponding month in moods dictionary.
+    """
+    print("Welcome to your mood tracker Aleksandra!\n We hope that you are doing well. Please enter how you are feeling based on the following scale:\n")
+    print("Sad | Irritable | Angry | Withdrawn | Sociable | Calm | Happy")
+    m = input("How are you feeling today?")
+    
+
+    
+
+
