b6eb3752f2292faeeed23c2c496085ff761aa68f
aleksandrakalas <aleksandrakalas2@gmail.com>
2023-01-03 23:22:51 -0500

    Starting the readline process of looping through txt file

--- starter.py
+++ starter.py
@@ -16,7 +16,7 @@ Completed: --/--/----
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 moods = {}
 
-def userInput(m: TextIOWrapper) -> None:
+def userInput(moodText: TextIOWrapper) -> None:
     """
     Ask for user input for their mood and add to the corresponding month in moods dictionary.
     """
@@ -25,6 +25,17 @@ def userInput(m: TextIOWrapper) -> None:
     m = input("How are you feeling today?")
     s = datetime.date.today().month
     
+    # file is open for reading
+    if moodText.readline() == datetime.date.today().year: # we are in the correct year
+        # loop through text looking for todays month
+        # check if we have already inputted a mood for today
+        # if we haven't  
+        pass 
+
+    else: # either wrong year or need to add "new" year file
+        pass
+
+    # This code will become irrelevant once the above is completed
     if months[s - 1] in moods.keys():
         moods[months[s - 1]].append(m)
     else:
@@ -37,7 +48,7 @@ def userInput(m: TextIOWrapper) -> None:
 def main() -> None:
     continuation = input("Would you like to open the mood tracker? Please enter Y for yes or N for no")
     while (continuation == 'Y' or continuation == 'y' or continuation == 'Yes' or continuation == 'yes'):
-        moodFile = open("moods.txt", "a")
+        moodFile = open("moods.txt")
         userInput(moodFile)
         continuation = input("Would you like to open the mood tracker? Please enter Y for yes or N for no")
     print('Have a nice day!')
